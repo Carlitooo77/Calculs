bfdde0b8f62f05baf5e5e5e421e402f6bea28646
carlf <carlf@DESKTOP-160ULHO>
2020-10-12 20:28:45 +0200

    t

--- Calculs/Program.cs
+++ Calculs/Program.cs
@@ -12,6 +12,7 @@ namespace Calculs
         static void Main(string[] args)
         {
             // changement dans le code
+            // et encore un peu
             // variables 
             Random rand = new Random(); // outil de génération de nombre aléatoire
             int val1, val2; // mémorisation de nombres aléatoires
